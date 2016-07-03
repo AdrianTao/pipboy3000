335f828b258d0988fd780005865f32f07c0786b6
AdrianTao <Adrian@taojundeMacBook-Air.local>
2016-07-03 17:02:23 +0800

    背景改为固定大小

--- pipboy3000.js
+++ pipboy3000.js
@@ -1,11 +1,13 @@
 function PipBoy(){
 	this.box = null;//容器
+	this.width = null;
+	this.height = null;
 }
 PipBoy.prototype = {
 	//初始化
 	init:function(){
 		this.box = document.getElementById('display');
-		
+
 	}
 }
 addLoadEvent(function(){
